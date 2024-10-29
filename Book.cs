ca9ba58c72922d6d629f741b6f4e223b76aa5e57
BaboucarrBouy <baboucarr.bouy@outlook.com>
2024-10-29 10:38:24 +0000

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrayMangingSystem
+{
+    internal class Book : LibraryItem
+    {
+        public string Author { get; set; }
+
+        public int ISBN { get; set; }
+
+        public Book(string title, int uniqueID, int publicationYear, int numberofCopies, string author, int iSBN) : base(title, uniqueID, publicationYear, numberofCopies)
+        {
+            Author = author;
+            ISBN = iSBN;
+        }
+
+        public override void DisplayInfo()
+        {
+
+                Console.WriteLine($"{Title} - ID:{UniqueID} - has a total of {NumberOfCopies} Copies Available");
+        }
+
+
+
+
+
+
+    }
+
+    
+
+}
